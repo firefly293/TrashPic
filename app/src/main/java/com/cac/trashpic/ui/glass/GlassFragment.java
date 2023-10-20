d960987d1df163a43b7c0e4427da28445f7009f0
firefly <advaitbadrish@gmail.com>
2023-10-20 16:56:46 -0700

    updates

--- app/src/main/java/com/cac/trashpic/ui/glass/GlassFragment.java
+++ app/src/main/java/com/cac/trashpic/ui/glass/GlassFragment.java
@@ -1,6 +1,7 @@
 package com.cac.trashpic.ui.glass;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
